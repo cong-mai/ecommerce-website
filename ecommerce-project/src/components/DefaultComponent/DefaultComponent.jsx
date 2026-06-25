608913208ceb2debd80b54b33a78327b46b78a7d
toan mai <ctmai@uno.edu>
2026-06-25 10:36:06 -0500

    fix: remove unused Children import in DefaultComponent

--- ecommerce-project/src/components/DefaultComponent/DefaultComponent.jsx
+++ ecommerce-project/src/components/DefaultComponent/DefaultComponent.jsx
@@ -1,4 +1,4 @@
-import React, { Children } from "react";  
+import React from "react";
 import HeaderComponent from "../HeaderComponet/HeaderComponent";
 
 const DefaultComponent = ({children}) => {
